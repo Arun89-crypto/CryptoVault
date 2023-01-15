2c1c872723c50f39d36894eec897ef252ee37056
Arun Jangra <arunjngra89@gmail.com>
2023-01-15 19:23:36 +0530

    feat : contract code completed

--- README.md
+++ README.md
@@ -41,3 +41,20 @@ CONCLUSION
 Here user had a profit and now owns much more worth of crypto that he/she/they owned it at time T1.
 
 ```
+
+## Currencies Supported
+
+The Vault supports ETH network and soon will support Cross chain.
+
+```txt
+ETH_ADDRESS_TESTNET = 0x7c1ed097af300c85f3e9aaf51a15de5c967f828e; // 1
+WBTC_ADDRESS_TESTNET = 0xc04b0d3107736c32e19f1c62b2af67be61d63a05; // 2
+LINK_ADDRESS_TESTNET = 0x326c977e6efc84e512bb9c30f76e30c160ed06fb; // 3
+USDT_ADDRESS_TESTNET = 0xe802376580c10fe23f027e1e19ed9d54d4c9311e; // 4
+DAI_ADDRESS_TESTNET = 0xdc31ee1784292379fbb2964b3b9c4124d8f89c60; // 5
+
+// * Address of price feeds
+ETH_PRICE = 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e; // 1
+WBTC_PRICE = 0xA39434A63A52E749F02807ae27335515BA4b07F7; // 2
+LINK_PRICE = 0x48731cF7e84dc94C5f84577882c14Be11a5B7456; // 3
+```
--- cryptovault-contracts/contracts/CryptoVault.sol
+++ cryptovault-contracts/contracts/CryptoVault.sol
@@ -1,66 +1,238 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract CryptoVault {
-    struct Vault {
-        address creator;
-        string name;
-        address[] users;
+// Imports
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract CryptoVault is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    // * Address of tokens
+    address public ETH_ADDRESS_TESTNET =
+        0x7c1ed097af300c85f3e9aaf51a15de5c967f828e; // 1
+    address public WBTC_ADDRESS_TESTNET =
+        0xc04b0d3107736c32e19f1c62b2af67be61d63a05; // 2
+    address public LINK_ADDRESS_TESTNET =
+        0x326c977e6efc84e512bb9c30f76e30c160ed06fb; // 3
+    address public USDT_ADDRESS_TESTNET =
+        0xe802376580c10fe23f027e1e19ed9d54d4c9311e; // 4
+    address public DAI_ADDRESS_TESTNET =
+        0xdc31ee1784292379fbb2964b3b9c4124d8f89c60; // 5
+
+    // * Address of price feeds
+    address public ETH_PRICE = 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e; // 101
+    address public WBTC_PRICE = 0xA39434A63A52E749F02807ae27335515BA4b07F7; // 102
+    address public LINK_PRICE = 0x48731cF7e84dc94C5f84577882c14Be11a5B7456; // 102
+
+    // * Items struct
+    struct Items {
+        IERC20 token; // 1 | 2 | 3
+        address withdrawer;
         uint256 amount;
+        uint256 depositedTokenType; // 1 | 2 | 3
+        uint256 unlockTimeStamp;
+        bool withdraw;
+        bool deposited;
+        uint256 tokensLoaned;
+        uint256 tokenTypeLoaned; // 4 | 5
     }
 
-    uint256 totalVaults;
-    mapping(uint256 => Vault) public vaults;
-    mapping(address => uint256) public balance;
+    // * Vars & Mappings
+    uint256 public depositsCount;
 
-    // EVENTS
-    event VaultDistribution(uint256 vaultId, uint256 amount);
+    mapping(address => uint256[]) public depositsByTokenAddress;
+    mapping(address => uint256[]) public depositsByWithdrawers;
+    mapping(uint256 => Items) public lockedToken;
+    mapping(address => mapping(address => uint256)) public walletTokenBalance;
+    mapping(uint256 => address) public tokenAddresses;
+    mapping(uint256 => address) public tokenPriceFeedAddresses;
 
-    constructor() {}
+    address public contractOwnerAddress;
+    AggregatorV3Interface internal priceFeed;
 
-    /*
-    memory : when we don't have to modify the stuff
-    storage : when we need to modify the stuff 
-    */
+    constructor() {
+        contractOwnerAddress = msg.sender;
 
-    function createVault(
-        string memory _name,
-        address[] memory _users,
-        uint256 _initialAmount
-    ) public returns (uint256 vaultId) {
-        Vault storage vault = vaults[totalVaults];
-        vault.creator = msg.sender;
-        vault.name = _name;
-        vault.users = _users;
-        vault.amount = _initialAmount;
-        totalVaults++;
-        return totalVaults - 1;
+        tokenAddresses[1] = ETH_ADDRESS_TESTNET;
+        tokenAddresses[2] = WBTC_ADDRESS_TESTNET;
+        tokenAddresses[3] = LINK_ADDRESS_TESTNET;
+        tokenAddresses[4] = USDT_ADDRESS_TESTNET;
+        tokenAddresses[5] = DAI_ADDRESS_TESTNET;
+
+        tokenPriceFeedAddresses[1] = ETH_PRICE;
+        tokenPriceFeedAddresses[2] = WBTC_PRICE;
+        tokenPriceFeedAddresses[3] = LINK_PRICE;
     }
 
-    function addAmount(uint256 _vaultId, uint256 _amount) public {
-        Vault storage vault = vaults[_vaultId];
+    // * EVENTS
+    event tokensLocked(
+        IERC20 _token,
+        address _withdrawer,
+        uint256 _amount,
+        IERC20 _outputToken,
+        uint256 _unlockTimestamp
+    );
+    event tokensWithdrawn(address _withdrawer, uint256 _amount);
+
+    /// @dev This is a function to lock the tokens in a vault and recieve a loan amount
+    /// @param _token Address of the token deposited
+    /// @param _withdrawer Address of the wallet who will be able to withdraw the amount
+    /// @param _amount Amount of token deposited
+    /// @param _outputToken Token type got as an output
+    /// @param _unlockTimestamp Timestamp at which vault will be available for withdraw
+    function lockTokens(
+        IERC20 _token,
+        address _withdrawer,
+        uint256 _amount,
+        uint256 _depositedTokenType,
+        IERC20 _outputToken,
+        uint256 _outputTokenType,
+        uint256 _unlockTimestamp
+    ) public payable returns (uint256 _id) {
+        // Checks
         require(
-            msg.sender == vault.creator,
-            "You need to be vault creator in order to add amount"
+            _unlockTimestamp < 10000000000,
+            "Unlock timestamp is not in seconds!"
         );
-        vault.amount += _amount;
+        require(
+            _unlockTimestamp > block.timestamp,
+            "Unlock timestamp is not in the future!"
+        );
+        require(
+            _token.allowance(msg.sender, address(this)) >= _amount,
+            "Approve tokens first!"
+        );
+        require(msg.value >= _amount, "Unsufficient Balance!");
+
+        // Transfering tokens from user to contract
+        _token.safeTransferFrom(msg.sender, address(this), _amount);
+        uint256 price = _getTokenPrice(_outputToken, _amount);
+        _outputToken.transfer(msg.sender, price);
+        // adding the token balance in the wallet of particular address
+        walletTokenBalance[address(_token)][msg.sender] = walletTokenBalance[
+            address(_token)
+        ][msg.sender].add(_amount);
+
+        _id = ++depositsCount;
+        lockedToken[_id].token = _token;
+        lockedToken[_id].withdrawer = _withdrawer;
+        lockedToken[_id].amount = _amount;
+        lockedToken[_id].depositedTokenType = _depositedTokenType;
+        lockedToken[_id].unlockTimestamp = _unlockTimestamp;
+        lockedToken[_id].withdrawn = false;
+        lockedToken[_id].deposited = true;
+        lockedToken[_id].tokensLoaned = price;
+        lockedToken[_id].tokenTypeLoaned = _outputTokenType;
+
+        BalanceOfVault[_depositedTokenType] += _amount;
+
+        depositsByTokenAddress[address(_token)].push(_id);
+        depositsByWithdrawers[_withdrawer].push(_id);
+        return _id;
     }
 
-    function distribute(uint256 _vaultId) public {
-        Vault storage vault = vaults[_vaultId];
+    /// @dev Function to get the latest price of the token in USD
+    /// @param _tokenAddress address of the token
+    /// @param _amount amount of the tokens deposited
+    function _getTokenPrice(uint256 _tokenType, uint256 _amount)
+        private
+        returns (uint256)
+    {
+        address aggregatorAddress;
+        priceFeed = AggregatorV3Interface(tokenPriceFeedAddresses[_tokenType]);
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        uint256 _price = uint256(answer * 10000000000);
+        return (_price * _amount) / 1000000000000000000;
+    }
+
+    /// @dev Function to withdraw the vault tokens
+    /// @param _id ID of the vault
+    function withdrawTokens(uint256 _id) external {
         require(
-            msg.sender == vault.creator,
-            "You need to be vault creator in order to add amount"
+            block.timestamp >= lockedToken[_id].unlockTimestamp,
+            "Tokens are still locked!"
         );
-        uint256 amountPerUser = vault.amount / vault.users.length;
-        if (vault.amount != 0) {
-            for (uint256 i; i < vault.users.length; i++) {
-                vault.amount -= amountPerUser;
-                balance[vault.users[i]] = amountPerUser;
-            }
-        } else {
-            revert("No funds Available");
-        }
-        emit VaultDistribution(_vaultId, amountPerUser * vault.users.length);
+        require(
+            msg.sender == lockedToken[_id].withdrawer,
+            "You are not the withdrawer!"
+        );
+        require(lockedToken[_id].deposited, "Tokens are not yet deposited!");
+        require(!lockedToken[_id].withdrawn, "Tokens are already withdrawn!");
+        require(
+            lockedToken[_id].withdrawer == msg.sender,
+            "You are not allowed to with draw to the tokens!"
+        );
+        require(
+            IERC20(tokenAddresses[lockedToken[_id].tokenTypeLoaned]) >=
+                lockedToken[_id].amount,
+            "You don't have enough balances to unlock your vault!"
+        );
+
+        lockedToken[_id].withdrawn = true;
+
+        walletTokenBalance[address(lockedToken[_id].token)][
+            msg.sender
+        ] = walletTokenBalance[address(lockedToken[_id].token)][msg.sender].sub(
+            lockedToken[_id].amount
+        );
+
+        // * Transfering funds from User => System
+
+        IERC20(tokenAddresses[lockedToken[_id].tokenTypeLoaned]).transfer(
+            address(this),
+            lockedToken[_id].amount
+        );
+
+        // * Transfering funds from System => User
+
+        IERC20(tokenAddresses[lockedToken[_id].depositedTokenType])
+            .safeTransferFrom(
+                msg.sender,
+                address(this),
+                lockedToken[_id].amount
+            );
+
+        emit Withdraw(msg.sender, lockedToken[_id].amount);
+    }
+
+    function getDepositsByTokenAddress(address _id)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        return depositsByTokenAddress[_id];
+    }
+
+    function getDepositsByWithdrawer(address _token, address _withdrawer)
+        external
+        view
+        returns (uint256)
+    {
+        return walletTokenBalance[_token][_withdrawer];
+    }
+
+    function getVaultsByWithdrawer(address _withdrawer)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        return depositsByWithdrawers[_withdrawer];
+    }
+
+    function getVaultById(uint256 _id) external view returns (Items memory) {
+        return lockedToken[_id];
+    }
+
+    function getTokenTotalLockedBalance(address _token)
+        external
+        view
+        returns (uint256)
+    {
+        return IERC20(_token).balanceOf(address(this));
     }
 }
