c6d2c5660f9f4560ecf0cbefaaccbf05f84a1550
Arun89-crypto <arunjngra89@gmail.com>
2023-04-20 11:17:04 +0530

    feat : added tags

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: ["./pages/**/*.js", "./Components/**/*.js"],
+    purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
     content: [
         './pages/**/*.{js,ts,jsx,tsx}',
         './Components/**/*.{js,ts,jsx,tsx}',
